46f02732ba6100dea8921e5e51f42640aebae027
Manu Web Dev <manudev@Manus-iMac-Pro.local>
2020-06-24 06:50:05 -0700

    Updates

---
+++ precache-manifest.66a736f894301d33e9b15605028c7fe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcebaf6a35688a0eff5df7c95dccaf46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01fef04f33b7df7f92f3",
+    "url": "./static/css/main.a14146af.chunk.css"
+  },
+  {
+    "revision": "6e551bd8bd91d5d6ab45",
+    "url": "./static/js/2.e3ac1b9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e3ac1b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01fef04f33b7df7f92f3",
+    "url": "./static/js/main.7d42688e.chunk.js"
+  },
+  {
+    "revision": "cc9a7ba9c9a63e4ad02d",
+    "url": "./static/js/runtime-main.cca146e6.js"
+  }
+]);
